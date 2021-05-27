72f434ba04f230bb81179ee03e529bc42dc5027d
Dan B <dbraunby@hotmail.com>
2021-05-27 11:52:00 -0600

    made site int pwa

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,33 @@
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-const webpack = require("webpack")
-const path = require("path");
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
 
-module.exports = {
-  entry:{
-    app: "./assets/js/script.js",
-    events: "./assets/js/event.js",
-    schedule: "./assets/js/schedule.js",
-    tickets: "./assets/js/tickets.js"
+const config = {
+  entry: {
+    app: './assets/js/script.js',
+    events: './assets/js/event.js',
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js'
   },
   output: {
-    filename: "[name].bundle.js",
-    path: __dirname + "/dist"
+    filename: '[name].bundle.js',
+    path: `${__dirname}/dist`
   },
   module: {
     rules: [
       {
-        test: /\.jpg$/i,
+        test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
             loader: 'file-loader',
             options: {
               esModule: false,
-              name (file) {
-                return "[Path][name].[ext]"
+              name(file) {
+                return '[path][name].[ext]';
               },
-              publicPath: function(url) {
-                return url.replace("../", "/assets/")
+              publicPath(url) {
+                return url.replace('../', '/assets/');
               }
             }
           },
@@ -38,13 +39,32 @@ module.exports = {
     ]
   },
   plugins: [
-  new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery"
-  }),
-  new BundleAnalyzerPlugin({
-    analyzerMode: "static",
-  })
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static'
+    }),
+    new WebpackPwaManifest({
+      name: 'Food Event',
+      short_name: 'Foodies',
+      description: 'An app that allows you to view upcoming food events.',
+      start_url: '../index.html',
+      background_color: '#01579b',
+      theme_color: '#ffffff',
+      fingerprints: false,
+      inject: false,
+      icons: [
+        {
+          src: path.resolve('assets/img/icons/icon-512x512.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons')
+        }
+      ]
+    })
   ],
-  mode: "development"
+  mode: 'development'
 };
+
+module.exports = config;
